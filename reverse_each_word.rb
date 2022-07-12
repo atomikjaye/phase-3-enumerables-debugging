04a145d7e223267df5dec2e4924ff3c9a1d0de6b
Jocelyn Naarden <tzionone@gmail.com>
2022-07-12 17:11:57 -0400

    Added Long and short versions as in the lesson

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 # Write your code here
 require 'pry'
 
-def reverse_each_word(sentence)
+def reverse_each_word_orig(sentence)
   words = sentence.split
   # Take each word in array
   indi = words.map &:reverse
@@ -11,4 +11,23 @@ def reverse_each_word(sentence)
   # add it to new array
 end
 
-puts reverse_each_word("Hello there, and how are you?")
+puts reverse_each_word_orig("Hello there, and how are you?")
+
+def reverse_each_word_long(sentence)
+  words = sentence.split
+  reversed_words = []
+
+  words.each do |word|
+    # Shovel reversed word into this variable
+    # Like .push()
+    reversed_words << word.reverse
+  end
+  reversed_words.join(" ")
+end
+
+puts reverse_each_word_long("Hello there, and how are you?")
+
+def reverse_each_word_Short(sentence)
+  reversed_words = sentence.split.map(&:reverse).join(" ")
+end
+puts reverse_each_word_Short("Hello there, and how are you?")
