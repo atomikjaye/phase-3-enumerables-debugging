b96e57666d901fc7c0b9746e643efd981c03fbd6
Jocelyn Naarden <tzionone@gmail.com>
2022-07-12 16:47:26 -0400

    My Original code

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,14 @@
 # Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+  words = sentence.split
+  # Take each word in array
+  indi = words.map &:reverse
+  indi.join(" ")
+  # binding.pry
+  # Reverse each word
+  # add it to new array
+end
+
+puts reverse_each_word("Hello there, and how are you?")
